90fbf58842353a4bf5bcf38b1c5e8bc06e4b0a53
aditiya105 <aditiyaagung105@gmail.com>
2022-02-08 15:23:32 +0700

    tambah query

--- Aditiya/data_apotek.sql
+++ Aditiya/data_apotek.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: 07 Feb 2022 pada 05.33
+-- Generation Time: 08 Feb 2022 pada 09.14
 -- Versi Server: 10.1.10-MariaDB
 -- PHP Version: 5.6.19
 
@@ -36,12 +36,16 @@ CREATE TABLE `data_jenis_obat` (
 --
 
 INSERT INTO `data_jenis_obat` (`id_jenis_obat`, `jenis_obat`) VALUES
-('1', 'pulvis'),
-('2', 'tablet'),
-('3', 'pil'),
-('4', 'kapsul'),
-('5', 'larutan'),
-('6', 'salep');
+('01', 'pulvis'),
+('02', 'tablet'),
+('03', 'pil'),
+('04', 'kapsul'),
+('05', 'larutan'),
+('06', 'salep'),
+('07', 'ekstrak'),
+('08', 'infusa'),
+('09', 'guttae'),
+('10', 'kaplet');
 
 -- --------------------------------------------------------
 
@@ -62,8 +66,16 @@ CREATE TABLE `data_obat` (
 --
 
 INSERT INTO `data_obat` (`id_obat`, `nama_obat`, `harga`, `stok`, `id_jenis_obat`) VALUES
-('bo01', 'bodrex', '1000', '5 pack', '2'),
-('bo02', 'bodrexin', '10000', '9 botol', '5');
+('bal01', 'balsem geliga', '5000', '10 pack', '06'),
+('bal02', 'balsem lang', '7000', '13 pack', '06'),
+('bo01', 'bodrex', '1000', '5 pack', '02'),
+('bo02', 'bodrexin', '10000', '9 botol', '05'),
+('bo03', 'bodrex extra', '1500', '7 pack', '10'),
+('bo12', 'bodrex flu dan batuk', '1500', '7 pack', '10'),
+('ins31', 'insto', '15000', '10 pack', '09'),
+('kap12', 'kapsul cannamix', '30000', '10 pack', '04'),
+('kom22', 'komik mint', '2000', '9 pack', '05'),
+('mas23', 'mastin', '20000', '5 pack', '07');
 
 -- --------------------------------------------------------
 
@@ -85,7 +97,16 @@ CREATE TABLE `data_pegawai` (
 --
 
 INSERT INTO `data_pegawai` (`id_pegawai`, `nama_pegawai`, `jenis_kelamin`, `alamat`, `tanggal_lahir`, `telepon`) VALUES
-('1', 'aditiya', 'laki-laki', 'graha walantaka', '1998-05-01', '087773773142');
+('1', 'aditiya', 'laki-laki', 'graha walantaka', '1998-05-01', '087773773142'),
+('10', 'krisna', 'laki-laki', 'taman pipitan indah', '1997-04-01', '089634691201'),
+('2', 'rizky', 'laki-laki', 'graha walantaka', '2004-08-31', '085789836912'),
+('3', 'lidya', 'perempuan', 'puri citra', '2005-04-20', '089645448967'),
+('4', 'canna', 'perempuan', 'puri citra', '2000-12-21', '083813987655'),
+('5', 'wawan', 'laki-laki', 'kiara rahayu', '1999-03-03', '087775529871'),
+('6', 'sinta', 'perempuan', 'taman pipitan indah', '1999-08-04', '086974634391'),
+('7', 'wardi', 'laki-laki', 'graha walantaka', '1997-03-24', '089675327429'),
+('8', 'luna', 'perempuan', 'graha walantaka', '1998-05-22', '087745657787'),
+('9', 'riska', 'perempuan', 'puri delta', '2000-12-10', '085218890901');
 
 -- --------------------------------------------------------
 
@@ -105,7 +126,16 @@ CREATE TABLE `data_pelanggan` (
 --
 
 INSERT INTO `data_pelanggan` (`id_pelanggan`, `nama_pelanggan`, `jenis_kelamin`, `alamat`) VALUES
-('11', 'agung', 'laki-laki', 'graha walantaka');
+('11', 'agung', 'laki-laki', 'graha walantaka'),
+('12', 'gunanto', 'laki-laki', 'puri delta'),
+('13', 'kirana', 'perempuan', 'taman pipitan indah'),
+('14', 'kayla', 'perempuan', 'taman pipitan indah'),
+('15', 'aldi', 'laki-laki', 'puri citra'),
+('16', 'cinta', 'perempuan', 'puri delta'),
+('17', 'dimas', 'laki-laki', 'bumi ciruas permai'),
+('18', 'pratama', 'laki-laki', 'bumi ciruas permai'),
+('19', 'abi', 'laki-laki', 'taman pipitan indah'),
+('20', 'guntur', 'laki-laki', 'graha walantaka');
 
 -- --------------------------------------------------------
 
@@ -129,7 +159,16 @@ CREATE TABLE `data_penjualan` (
 --
 
 INSERT INTO `data_penjualan` (`kode_transaksi`, `tanggal`, `id_pelanggan`, `id_pegawai`, `id_obat`, `id_jenis_obat`, `jumlah_obat`, `total`) VALUES
-('21', '2022-02-07', '11', '1', 'bo02', '5', '1', '10000');
+('21', '2022-02-07', '11', '1', 'bo02', '05', '1', '10000'),
+('22', '2021-12-07', '15', '1', 'kom22', '05', '3', '6000'),
+('23', '2021-12-27', '13', '3', 'ins31', '09', '1', '15000'),
+('24', '2022-01-13', '14', '2', 'bo01', '02', '6', '6000'),
+('25', '2021-12-15', '12', '2', 'ins31', '09', '1', '15000'),
+('26', '2012-12-20', '14', '3', 'ins31', '09', '2', '30000'),
+('27', '2022-02-19', '20', '5', 'kom22', '05', '5', '10000'),
+('28', '2021-12-20', '11', '7', 'bo01', '02', '9', '9000'),
+('29', '2021-11-15', '11', '1', 'ins31', '09', '2', '30000'),
+('30', '2021-11-01', '14', '7', 'ins31', '09', '1', '15000');
 
 --
 -- Indexes for dumped tables
---
+++ Lamidi/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>     
+<html>
+    <head>
+        <title>
+            Halo
+        </title>
+    </head>
+    <body>
+      Hai Nama saya Lamidi
+    </body>
+</html>
