6e6d0b3549770ff31232595c707416badb5b2916
sadikins <msadkin1989@gmail.com>
2022-02-08 08:50:45 +0700

    add new folder

---
+++ Sadikins/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sadikin</title>
+</head>
+<body>
+
+	Ini adalah folder Sadikin
+	
+</body>
+</html>
