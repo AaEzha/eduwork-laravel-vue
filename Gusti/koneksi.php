37683b77ad3e65036deaf2f2cf5a172f73b906de
@GustiNugraha <nugraha.agustiawan12@gmail.com>
2022-02-08 21:40:15 +0700

    task query PHP - Database

--- Gusti/koneksi.php
+++ Gusti/koneksi.php
@@ -15,14 +15,14 @@ if(!$conn) {
 // echo "Connection Successfully";
 // mysqli_close($conn);
 
-$sql = "SELECT * FROM buku";
+$sql = "SELECT * FROM anggota where alamat LIKE '%jakarta%'";
 $result = $conn ->query($sql);
 
 //output data of each row
 if($result -> num_rows > 0) {
 
     while ($row = $result -> fetch_assoc()) {
-        echo "Buku : ". $row ["isbn"] . " " . $row ["judul"]. "<br>";
+        echo "Anggota : ". $row ["nama"] . " , " . $row ["alamat"]. " ." . $row ["email"]."<br>";
     }
 } else {
     echo "0 Result";
