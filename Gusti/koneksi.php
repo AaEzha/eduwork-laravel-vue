301fcca6b526d2bd5b9d2e7c1c3b80e093e1b831
@GustiNugraha <nugraha.agustiawan12@gmail.com>
2022-02-08 21:25:27 +0700

    modify koneksi

--- Gusti/koneksi.php
+++ Gusti/koneksi.php
@@ -12,7 +12,22 @@ if(!$conn) {
     die ( "connection failed: " . mysqli_connect_error());
 }
 
-echo "Connection Successfully";
-mysqli_close($conn);
+// echo "Connection Successfully";
+// mysqli_close($conn);
+
+$sql = "SELECT * FROM buku";
+$result = $conn ->query($sql);
+
+//output data of each row
+if($result -> num_rows > 0) {
+
+    while ($row = $result -> fetch_assoc()) {
+        echo "Buku : ". $row ["isbn"] . " " . $row ["judul"]. "<br>";
+    }
+} else {
+    echo "0 Result";
+}
+$conn->close();
+
 
 ?>
