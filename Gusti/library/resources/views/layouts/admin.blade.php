7b4fdacb5fcf0ccb38bd510bb24146793c14ec65
@GustiNugraha <nugraha.agustiawan12@gmail.com>
2022-02-23 05:38:34 +0700

    nambah nav

--- Gusti/library/resources/views/layouts/admin.blade.php
+++ Gusti/library/resources/views/layouts/admin.blade.php
@@ -26,11 +26,11 @@
 
         <!-- Preloader -->
         <div class="preloader flex-column justify-content-center align-items-center">
-            <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">
+            <img class="animation__shake" src="{{ asset ('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTELogo" height="60" width="60">
         </div>
 
         <!-- Navbar -->
-        <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <nav class="main-header navbar navbar-expand navbar-danger navbar-dark">
             <!-- Left navbar links -->
             <ul class="navbar-nav">
                 <li class="nav-item">
@@ -101,14 +101,16 @@
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();"> 
+                        <i class="fas fa-arrow-right"></i>
                         {{ __('Logout') }}
                     </a>
 
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                         @csrf
                     </form>
+                    
                 </li>
 
             </ul>
@@ -116,11 +118,11 @@
         <!-- /.navbar -->
 
         <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <aside class="main-sidebar sidebar-dark-danger elevation-4">
             <!-- Brand Logo -->
-            <a href="index3.html" class="brand-link">
-                <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">Perpustakaan</span>
+            <a href="#" class="brand-link">
+                <img src="{{ asset ('assets/dist/img/AdminLTELogo.png') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+                <span class="brand-text font-weight-light">Reading Room</span>
             </a>
 
             <!-- Sidebar -->
@@ -128,7 +130,7 @@
                 <!-- Sidebar user panel (optional) -->
                 <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                     <div class="image">
-                        <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+                        <img src="{{ asset ('assets/dist/img/user2-160x160.jpg') }}" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
                         <a href="#" class="d-block">{{ auth ()->user()->name }}</a>
@@ -158,6 +160,27 @@
                                 <p>
                                     Home
                                     <i class="right fas fa-angle-left"></i>
+                                </p>
+                            </a>
+                        </li>
+                        <li class="nav-item ">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-book"></i>
+                                <!-- <i class="fa-solid fa-book-open-cover"></i> -->
+                                <p>
+                                    Data Buku
+                                    <i class="right fas fa-angle-down"></i>
+                                </p>
+                            </a>
+                        </li>
+                      
+                        <li class="nav-item ">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-solid fa-users"></i>
+                                <!-- <i class="fa-solid fa-book-open-cover"></i> -->
+                                <p>
+                                    Data Anggota
+                                    <i class="right fas fa-angle-down"></i>
                                 </p>
                             </a>
                         </li>
