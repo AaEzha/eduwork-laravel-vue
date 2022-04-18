17c49b9e6a3c7bdaa26c2ae13c2320268026058c
irwan firdaus <irwanfir1@gmail.com>
2022-04-18 21:21:19 +0700

    elequent relationship model

--- irwan/library/app/Http/Controllers/HomeController.php
+++ irwan/library/app/Http/Controllers/HomeController.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
+use App\Models\Member;
+use App\Models\Publisher;
+use App\Models\Catalog;
+use App\Models\Author;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +28,15 @@ class HomeController extends Controller
      */
     public function index()
     {
+        // $members = Member::with('user')->get();
+        // $books = Book::with('publisher')->get();
+        // $publishers = Publisher::with('books')->get();
+         // $catalogs = Catalog::with('books')->get();
+        $authors = Author::with('books')->get();
+
+         return $authors;
+        // return $publishers;
+        // return $members;
         return view('home');
     }
 }
--- irwan/library/app/Models/Book.php
+++ irwan/library/app/Models/Book.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Book extends Model
 {
     use HasFactory;
+
+     public function author()
+    {
+    	// return $this->belongsTo('App\Models\Publisher', 'publisher_id');
+    	// return $this->belongsTo('App\Models\Catalog', 'catalog_id');
+    	return $this->belongsTo('App\Models\Author', 'author_id');
+    }
 }
