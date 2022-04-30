a44447a8294ec1d4a6e690eae838a4e3cb423f76
aditiya105 <aditiyaagung105@gmail.com>
2022-05-01 01:20:14 +0700

    migration and model

--- Aditiya/library/database/migrations/2022_04_30_170224_create_transactions_table.php
+++ Aditiya/library/database/migrations/2022_04_30_170224_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->unsignedBigInteger('member_id')->nullabel();
+            $table->unsignedBigInteger('member_id');
+            $table->date('date_start');
+            $table->date('date_end');
             $table->timestamps();
 
             $table->foreign('member_id')->references('id')->on('members');
@@ -34,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('transactions');
     }
 }
