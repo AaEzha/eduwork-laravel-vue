7b47854edcce95f510e51908eec4c1c4f1e89a3e
aditiya105 <aditiyaagung105@gmail.com>
2022-05-01 16:18:58 +0700

    migration and model

--- Aditiya/library/database/migrations/2014_10_12_000000_create_users_table.php
+++ Aditiya/library/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,10 +21,10 @@ class CreateUsersTable extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->unsignedBigInteger('member_id')->nullable();
+
             $table->timestamps();
 
-            $table->foreign('member_id')->references('id')->on('members');
+
         });
     }
 
