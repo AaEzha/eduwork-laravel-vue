4a4f6bf5c6fd3776b014f155738816fe5bd1c35b
sawindri <sawindriindri.com>
2022-05-14 03:01:56 +0700

    migrations and model

--- perpustakaan/database/migrations/2022_05_13_193531_create_publishers_table.php
+++ perpustakaan/database/migrations/2022_05_13_193531_create_publishers_table.php
@@ -13,17 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('publishers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->unsignedBigInteger('member_id')->nullable();
+            $table->string('name', 64);
+            $table->string('email', 64);
+            $table->char('phone_number', 15);
+            $table->text('address');
             $table->timestamps();
-
-            $table->foreign('member_id')->references('id')->on('members');
         });
     }
 
@@ -34,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('publishers');
     }
 };
--- perpustakaan/resources/views/layouts/admin.blade.php
+++ perpustakaan/resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>AdminLTE 3 | Dashboard</title>
+  <title>Perpustakaan | Dashboard</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
@@ -148,12 +148,6 @@
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0">Dashboard</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Dashboard v1</li>
-            </ol>
           </div><!-- /.col -->
         </div><!-- /.row -->
       </div><!-- /.container-fluid -->
