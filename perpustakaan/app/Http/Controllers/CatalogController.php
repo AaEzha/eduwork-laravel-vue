094dd101646e88cfb9dcca5e7e85ec225ab3e8a0
sawindri <sawindriindri.com>
2022-05-17 01:49:00 +0700

    controller

---
+++ perpustakaan/app/Http/Controllers/CatalogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Catalog;
+use Illuminate\Http\Request;
+
+class CatalogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Catalog  $catalog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Catalog $catalog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Catalog  $catalog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Catalog $catalog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Catalog  $catalog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Catalog $catalog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Catalog  $catalog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Catalog $catalog)
+    {
+        //
+    }
+}
